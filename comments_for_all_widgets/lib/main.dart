91d7936f9de1df095e6ceb85273fc46023e8598f
adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-02-25 14:36:16 +0300

    done

--- comments_for_all_widgets/lib/main.dart
+++ comments_for_all_widgets/lib/main.dart
@@ -55,6 +55,7 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
+    // setState бул функция. setState чакырылганда build метод кайра иштейт
     setState(() {
       _counter++;
     });
