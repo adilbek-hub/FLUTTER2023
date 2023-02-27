4ccaee029ef887979e0732cd02d30787cefb2717
adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-02-27 15:38:34 +0300

    done

---
+++ weather_app_data_alyp_keluu_model_tuzuu/android/app/src/main/kotlin/com/example/weather_app_data_alyp_keluu_model_tuzuu/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.weather_app_data_alyp_keluu_model_tuzuu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ weather_app_data_alyp_keluu_model_tuzuu/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app_data_alyp_keluu_model_tuzuu/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
