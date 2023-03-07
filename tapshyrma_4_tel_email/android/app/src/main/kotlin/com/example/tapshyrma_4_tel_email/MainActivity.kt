4619a085002b590526b0640869e97b100dd5f7c9
adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-03-07 22:39:09 +0300

    done

---
+++ tapshyrma_4_tel_email/android/app/src/main/kotlin/com/example/tapshyrma_4_tel_email/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tapshyrma_4_tel_email
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ tapshyrma_4_tel_email/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:tapshyrma_4_tel_email/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
