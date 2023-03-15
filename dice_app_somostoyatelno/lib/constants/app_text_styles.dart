b48b72dcafb862b2e7f20022aa1923d1d55d5109
adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-03-15 17:01:34 +0300

    done

---
+++ dice_app_somostoyatelno/lib/constants/app_text_styles.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const diceAppStyle = TextStyle(
+    color: Colors.green,
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    fontStyle: FontStyle.italic,
+  );
+
+  static const diceAppStyle2 = TextStyle(
+    color: Color.fromARGB(255, 253, 240, 121),
+    fontSize: 30,
+    fontWeight: FontWeight.w700,
+    fontStyle: FontStyle.normal,
+  );
+}
---
+++ dice_app_somostoyatelno/lib/constants/text_colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class TextColors {
+  static const Color appBarTextColor = Colors.black;
+}
