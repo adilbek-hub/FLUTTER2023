3e6e9245f02de92b85e211bf10dd7226c88d0561
adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-03-30 23:28:31 +0300

    do

--- coffee_shop/lib/pages/home.dart
+++ coffee_shop/lib/pages/home.dart
@@ -12,20 +12,28 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: Container(
-        decoration: const BoxDecoration(
-          color: Colors.white,
-        ),
-        child: GNav(mainAxisAlignment: MainAxisAlignment.center, tabs: [
-          GButton(
-            icon: Icons.home,
-            text: 'home',
-          ),
-          GButton(
-            icon: Icons.phone,
-            text: 'phone',
-          ),
-        ]),
+      backgroundColor: Colors.grey[300],
+      body: Column(
+        children: [],
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(bottom: 10),
+        child: GNav(
+            color: Colors.grey[400],
+            activeColor: Colors.grey[200],
+            tabBorderRadius: 25,
+            tabBackgroundColor: Colors.grey.shade600,
+            mainAxisAlignment: MainAxisAlignment.center,
+            tabs: const [
+              GButton(
+                icon: Icons.home,
+                text: 'home',
+              ),
+              GButton(
+                icon: Icons.phone,
+                text: 'phone',
+              ),
+            ]),
       ),
     );
   }
