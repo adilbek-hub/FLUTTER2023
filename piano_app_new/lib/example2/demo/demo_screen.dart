f4eef45645e7b65bae9b0fea784e4540cc552a26
adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-04-03 16:18:37 +0300

    do

--- piano_app_new/lib/example2/demo/demo_screen.dart
+++ piano_app_new/lib/example2/demo/demo_screen.dart
@@ -22,75 +22,46 @@ class PianoApp2 extends StatefulWidget {
 }
 
 class _PianoApp2State extends State<PianoApp2> {
+  bool isPressed = false;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: SizedBox(
-        child: ListView.builder(
-            itemCount: notes.length,
-            scrollDirection: Axis.horizontal,
-            itemBuilder: (BuildContext contex, index) {
-              return GestureDetector(
-                onTap: () {
-                  AudioPlayer().play(AssetSource(notes[index]));
-                },
-                child: Container(
-                  margin: EdgeInsets.only(left: 5),
-                  height: 80,
-                  color: Colors.green,
-                  child: Text('go'),
-                ),
-              );
-            }),
-      ),
-    );
-  }
-}
-
-//Кара кнопканын баскычы
-
-// Ак Кнопканын баскычы
-/*
-class DemoWhiteKey extends StatefulWidget {
-  const DemoWhiteKey({super.key});
-
-  @override
-  State<DemoWhiteKey> createState() => _DemoWhiteKeyState();
-}
-
-class _DemoWhiteKeyState extends State<DemoWhiteKey> {
-  bool isPressed = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTapDown: (details) {
-        setState(() {
-          isPressed = true;
-        });
-      },
-      onTapUp: (details) {
-        setState(() {
-          isPressed = false;
-        });
-      },
-      onTap: () {
-        playNoteAudio(context, note.asset);
-        //  AudioPlayer().play(AssetSource('$nota.mp3'));
-        // final player = AudioPlayer();
-        // player.play(AssetSource(''));
-      },
-      child: Transform(
-        transform: Matrix4.rotationX(isPressed ? 0.2 : 0),
-        child: Container(
-          decoration: BoxDecoration(
-              border: Border.all(color: Colors.black, width: 3),
-              color: isPressed ? Colors.grey[300] : Colors.white),
-          width: 80,
-          height: double.infinity,
+        child: Stack(
+          children: [
+            ListView.builder(
+                itemCount: notes.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (BuildContext contex, index) {
+                  return GestureDetector(
+                    onTapDown: (details) {
+                      setState(() {
+                        isPressed = true;
+                      });
+                      final notalar = notes[index];
+                      AudioPlayer().play(
+                        AssetSource(
+                          notalar,
+                        ),
+                      );
+                    },
+                    onTapUp: (details) {
+                      setState(() {
+                        isPressed = false;
+                      });
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.only(left: 6),
+                      height: 80,
+                      width: 70,
+                      color: Colors.white,
+                      // child: const Text('go'),
+                    ),
+                  );
+                }),
+          ],
         ),
       ),
     );
   }
 }
-*/
