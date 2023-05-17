88fc8a39757672658b89b6196cbbb57b48c7d2a8
adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-05-17 21:50:42 +0300

    do

---
+++ data_with_dio/lib/constants/api_const.dart
@@ -0,0 +1,5 @@
+class ApiConst {
+  static String getIcon(String iconData, int sizeIcon) {
+    return 'https://openweathermap.org/img/wn/$iconData@${sizeIcon}x.png';
+  }
+}
--- data_with_dio/lib/main.dart
+++ data_with_dio/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:data_with_dio/second_variant.dart';
+import 'package:data_with_dio/third_variant.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const SecondMyHomePage(),
+      home: const ThirdMyHomePage(),
     );
   }
 }
