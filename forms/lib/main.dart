d4f2c964b34a32c8498fca6d8324886bb11b5605
adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-05-26 18:26:08 +0300

    do

--- forms/lib/main.dart
+++ forms/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:forms/focus%20_text_fields.dart';
 import 'package:forms/form_validate.dart';
 import 'package:forms/style_text_field.dart';
 
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const StyleTextField(),
+      home: const FocusTextField(),
     );
   }
 }
