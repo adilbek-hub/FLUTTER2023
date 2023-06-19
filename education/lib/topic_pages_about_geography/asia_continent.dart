a18a30c4e8a9aa6804da1d7af68eb58d06e0bb63
adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-06-19 22:40:44 +0300

    do

--- education/lib/topic_pages_about_geography/asia_continent.dart
+++ education/lib/topic_pages_about_geography/asia_continent.dart
@@ -77,6 +77,46 @@ class AsiaContinenti extends StatelessWidget {
             const Text(
               'Азиянын аймагы ушунчалык чоң болгондуктан, саясатчылар, журналисттер же окумуштуулар аны кээде Жакынкы Чыгыш, Батыш Азия жана Ыраакы Чыгыш деп бөлүшөт. Бирок, географиялык жактан Азия туура 5 аймакка бөлүнөт:',
             ),
+            const SizedBox(height: 5),
+            SizedBox(
+              height: 80,
+              width: 300,
+              child: Card(
+                color: Colors.red[300],
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Урматтуу окуучу!! Тест сынагынан өтүңүз.',
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 10),
+                    SizedBox(
+                      width: 100,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xff42d792),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(13.0),
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.push<void>(
+                            context,
+                            MaterialPageRoute<void>(
+                              builder: (BuildContext context) =>
+                                  const AsiaContinenti(),
+                            ),
+                          );
+                        },
+                        child: const Text('ТЕСТ'),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
