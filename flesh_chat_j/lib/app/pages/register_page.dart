0d69f0a5da3daa2f94c30fee76be757c6d733dea
adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-06-20 20:51:34 +0300

    do

---
+++ flesh_chat_j/lib/app/pages/register_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegisterView extends StatelessWidget {
+  const RegisterView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('RegisterPage'),
+      ),
+    );
+  }
+}
---
+++ flesh_chat_j/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flesh_chat_j/app/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
