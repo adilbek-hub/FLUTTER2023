820c7857598e0053baf04089e3ea206d228a8eea
Adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-07-24 16:54:34 +0600

    do

---
+++ flesh_chat_j/lib/app/routes/app_pages.dart
@@ -0,0 +1,40 @@
+import 'package:flesh_chat_j/app/modules/login/views/register_view.dart';
+import 'package:get/get.dart';
+
+import '../modules/flash_chat/bindings/flash_chat_binding.dart';
+import '../modules/flash_chat/views/flash_chat_view.dart';
+import '../modules/home/bindings/home_binding.dart';
+import '../modules/home/views/home_view.dart';
+import '../modules/login/bindings/login_binding.dart';
+import '../modules/login/views/login_view.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  AppPages._();
+
+  static const INITIAL = Routes.FLASH_CHAT;
+
+  static final routes = [
+    GetPage(
+      name: _Paths.HOME,
+      page: () => const HomeView(),
+      binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.LOGIN,
+      page: () => const LoginView(),
+      binding: LoginBinding(),
+    ),
+    GetPage(
+      name: _Paths.REGISTER,
+      page: () => const RegisterView(),
+      binding: LoginBinding(),
+    ),
+    GetPage(
+      name: _Paths.FLASH_CHAT,
+      page: () => const FlashChatView(),
+      binding: FlashChatBinding(),
+    ),
+  ];
+}
