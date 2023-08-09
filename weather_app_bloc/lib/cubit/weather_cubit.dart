cbd2318ed377922806b3764f30fea0ecf34026a8
Adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-08-09 11:08:35 +0600

    done

---
+++ weather_app_bloc/lib/cubit/weather_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:weather_app_bloc/model/weather_response.dart';
+import 'package:weather_app_bloc/servises/fetch_weather.dart';
+
+part 'weather_state.dart';
+
+class WeatherCubit extends Cubit<WeatherState> {
+  WeatherCubit(
+    this.service,
+  ) : super(WeatherInitial());
+
+  final WeatherService service;
+
+  Future<void> getWeatherByCityName(String cityName) async {
+    emit(WeatherLoading());
+    final data = await service.getWeatherByCityName(cityName);
+    if (data != null) {
+      emit(WeatherSuccess(data));
+    } else {
+      emit(WeatherError(errorText: 'Bir kata boldy'));
+    }
+  }
+}
