4da81be7e972da1d5d70cec3019cb514906e40d1
Adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-09-20 18:16:23 +0600

    done

--- education/lib/views/biology_topics_view.dart
+++ education/lib/views/biology_topics_view.dart
@@ -1,15 +1,6 @@
-import 'package:auto_size_text/auto_size_text.dart';
-import 'package:drop_cap_text/drop_cap_text.dart';
-import 'package:education/model/subjects.dart';
-import 'package:education/pages/topic_pages_about_biology/kishsi_jana_janybar.dart';
-import 'package:education/pages/topic_pages_about_biology/kletka.dart';
-import 'package:education/pages/topic_pages_about_biology/mee.dart';
-import 'package:education/pages/topic_pages_about_biology/nerv_sistemasy.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
-
 import '../bloc/education_bloc.dart';
 import '../components/biology_bolumu/biology_bolumu_bloc.dart';
 
