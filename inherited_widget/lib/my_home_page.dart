1f71b8a0b937cae30ed9289d018cac8d2e433cd0
Adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-10-01 18:33:51 +0600

    done

---
+++ inherited_widget/lib/my_home_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class Example extends StatelessWidget {
+  const Example({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(child: DataOwnerStateFull()),
+    );
+  }
+}
+
+class DataOwnerStateFull extends StatefulWidget {
+  const DataOwnerStateFull({super.key});
+
+  @override
+  State<DataOwnerStateFull> createState() => _DataOwnerStateFullState();
+}
+
+class _DataOwnerStateFullState extends State<DataOwnerStateFull> {
+  var _value = 0;
+
+  void _increment() {
+    _value += 1;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: () => _increment(),
+          child: const Text('Жми'),
+        ),
+        const DataConsumerStateless(),
+      ],
+    );
+  }
+}
+
+class DataConsumerStateless extends StatelessWidget {
+  const DataConsumerStateless({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final value =
+        context.findAncestorStateOfType<_DataOwnerStateFullState>()?._value ??
+            0;
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text('$value'),
+          const DataConsumerStateFull(),
+        ],
+      ),
+    );
+  }
+}
+
+class DataConsumerStateFull extends StatefulWidget {
+  const DataConsumerStateFull({super.key});
+
+  @override
+  State<DataConsumerStateFull> createState() => _DataConsumerStateFullState();
+}
+
+class _DataConsumerStateFullState extends State<DataConsumerStateFull> {
+  @override
+  Widget build(BuildContext context) {
+    final _value =
+        context.findAncestorStateOfType<_DataOwnerStateFullState>()?._value ??
+            0;
+    return Text('$_value');
+  }
+}
