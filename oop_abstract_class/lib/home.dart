53e54763d2cf48835b8378a2f3e7c164841e544d
Adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-10-02 00:17:21 +0600

    done

--- oop_abstract_class/lib/home.dart
+++ oop_abstract_class/lib/home.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:oop_abstract_class/model.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final Transport transport = Car();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Информация о транспорте'),
+      ),
+      body: const Column(children: []),
+    );
   }
 }
