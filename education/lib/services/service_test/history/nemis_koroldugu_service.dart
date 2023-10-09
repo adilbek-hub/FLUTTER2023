60c28fd639c000c04313337f8ec4ab68edd513c3
Adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-10-09 19:57:11 +0600

    done

---
+++ education/lib/services/service_test/history/nemis_koroldugu_service.dart
@@ -0,0 +1,28 @@
+import 'package:education/model/model_test/geography_test/asia.dart';
+import 'package:education/model/model_test/history/nemis_koroldugu.dart';
+import 'package:http/http.dart' as http;
+
+class NemisKorolduguTestTopicsService {
+  const NemisKorolduguTestTopicsService({required this.client});
+  final http.Client client;
+  Future<List<NemisKorolduguTestToicsModel>?> getData() async {
+    try {
+      final response = await http.get(Uri.parse(
+          'https://adilbek-hub.github.io/my_data/tests_data/geography/nemis_koroldugu.json'));
+
+      if (response.statusCode == 200) {
+        final data = nemisKorolduguTestToicsModelFromJson(response.body);
+        return data;
+      } else {
+        throw Exception('Failed to load computer data');
+      }
+    } catch (e) {
+      print('Кештен ката: ${e.toString()}');
+    }
+    return null;
+  }
+}
+
+final nemisKorolduguTestTopicsService = NemisKorolduguTestTopicsService(
+  client: http.Client(),
+);
