52144d0107ce53827aafbf5625946d5217087bcb
Adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-10-10 16:48:49 +0600

    done

---
+++ education/lib/services/service_test/history/bayirky_germandar.dart
@@ -0,0 +1,28 @@
+import 'package:education/model/model_test/history/bayrky_germandar.dart';
+import 'package:education/model/model_test/history/rim.dart';
+import 'package:http/http.dart' as http;
+
+class BayirkyGermandarTestTopicsService {
+  const BayirkyGermandarTestTopicsService({required this.client});
+  final http.Client client;
+  Future<List<BayrkGermandarTestToicsModel>?> getData() async {
+    try {
+      final response = await http.get(Uri.parse(
+          'https://adilbek-hub.github.io/my_data/tests_data/history/bayrky_germandar.json'));
+
+      if (response.statusCode == 200) {
+        final data = bayrkGermandarTestToicsModelFromJson(response.body);
+        return data;
+      } else {
+        throw Exception('Failed to load computer data');
+      }
+    } catch (e) {
+      print('Кештен ката: ${e.toString()}');
+    }
+    return null;
+  }
+}
+
+final bayirkyGermandyktarTestTopicsService = BayirkyGermandarTestTopicsService(
+  client: http.Client(),
+);
