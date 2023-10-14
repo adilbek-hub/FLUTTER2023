85dd7c65e77bf4241c0b529f4433cc3a31f7161a
Adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-10-14 21:59:55 +0600

    done

---
+++ bloc_megacom50_part3/lib/view/home_view.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class NomeView extends StatefulWidget {
+  const NomeView({super.key});
+
+  @override
+  State<NomeView> createState() => _NomeViewState();
+}
+
+class _NomeViewState extends State<NomeView> {
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController controllerName = TextEditingController();
+    final TextEditingController controllerMessage = TextEditingController();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: controllerName,
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'Name'),
+            ),
+            const SizedBox(height: 25),
+            TextField(
+              controller: controllerMessage,
+              maxLines: 6,
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'Message'),
+            ),
+            const SizedBox(height: 25),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.brown,
+                  minimumSize: const Size(double.infinity, 30)),
+              onPressed: () {},
+              child: const Text('Send'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
