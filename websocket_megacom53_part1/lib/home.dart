e5639cf27452c000c88c5c409b7bfb0bf7051b5a
Adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-10-17 22:46:15 +0600

    done

--- websocket_megacom53_part1/lib/home.dart
+++ websocket_megacom53_part1/lib/home.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
+import 'package:chart_sparkline/chart_sparkline.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 class Home extends StatefulWidget {
@@ -13,6 +15,7 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   String name = '';
   String price = '';
+  List<double> data = [];
   webSocketFun() async {
     print('object');
     final wsUrl = Uri.parse('wss://stream.binance.com:9443/ws/etheur@trade');
@@ -25,8 +28,10 @@ class _HomeState extends State<Home> {
       print(valueMap.runtimeType);
       name = valueMap["s"];
       price = valueMap["p"];
+      data.add(double.tryParse(price) ?? 0.0);
       print(name);
       print(price);
+      print(data);
       setState(() {});
     });
   }
@@ -60,6 +65,15 @@ class _HomeState extends State<Home> {
                 color: Colors.white,
                 fontWeight: FontWeight.bold,
               ),
+            ),
+            Sparkline(
+              data: data,
+              gridLinelabelPrefix: '\$',
+              gridLineLabelPrecision: 3,
+              enableGridLines: true,
+              fillMode: FillMode.below,
+              fillColor: Colors.red,
+              pointsMode: PointsMode.all,
             ),
           ],
         ),
