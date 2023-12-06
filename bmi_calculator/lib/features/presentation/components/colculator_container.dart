09627ae6863f6e30d4fc4037ecd88ba68ab1e880
Adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-12-06 22:04:42 +0600

    done

--- bmi_calculator/lib/features/presentation/components/colculator_container.dart
+++ bmi_calculator/lib/features/presentation/components/colculator_container.dart
@@ -3,20 +3,22 @@ import 'package:bmi_calculator/features/presentation/pages/constants/app_text_st
 import 'package:flutter/material.dart';
 
 class CalcualtorContainer extends StatelessWidget {
-  const CalcualtorContainer({
-    super.key,
-  });
+  const CalcualtorContainer({super.key, this.onTap});
+  final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 73,
-      color: AppColors.buttonContainerColor,
-      child: Center(
-        child: Text(
-          'Calculator'.toUpperCase(),
-          style: AppTextStyles.bmiTextStyle,
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        width: double.infinity,
+        height: 73,
+        color: AppColors.buttonContainerColor,
+        child: Center(
+          child: Text(
+            'Calculator'.toUpperCase(),
+            style: AppTextStyles.bmiTextStyle,
+          ),
         ),
       ),
     );
--- bmi_calculator/lib/features/presentation/pages/my_home_page.dart
+++ bmi_calculator/lib/features/presentation/pages/my_home_page.dart
@@ -6,6 +6,7 @@ import 'package:bmi_calculator/features/presentation/pages/constants/app_colors.
 import 'package:bmi_calculator/features/presentation/pages/constants/app_sized_box.dart';
 import 'package:bmi_calculator/features/presentation/pages/constants/app_text_styles.dart';
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -109,7 +110,52 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      bottomNavigationBar: const CalcualtorContainer(),
+      bottomNavigationBar: CalcualtorContainer(
+        onTap: () {
+          final res = weight / math.pow(slider / 100, 2);
+          if (res <= 18.0) {
+            dialogBuilder(context, 'Aryk');
+            print('aryk');
+          } else if (res >= 18.5 && res <= 24.9) {
+            dialogBuilder(context, 'Norma');
+            print('norma');
+          } else if (res >= 25) {
+            dialogBuilder(context, 'Semiz');
+            print('semiz');
+          } else {
+            dialogBuilder(context, 'Oto semiz');
+            print('joop jok');
+          }
+        },
+      ),
+    );
+  }
+
+  Future<void> dialogBuilder(BuildContext context, String text) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: AppColors.appBarBgc,
+          title: const Center(child: Text('bmi RESULT')),
+          content: Text(
+            text,
+            style: AppTextStyles.bmiTextStyle,
+            textAlign: TextAlign.center,
+          ),
+          actions: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('dfdf'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 
